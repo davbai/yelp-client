a2c19947c8060036ee94da05c1b377378c68fc63
David Bai <dbai@quantcast.com>
2014-09-21 17:20:38 -0700

    Initial commit

---
+++ Yelp/BusinessTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BusinessTableViewCell.swift
+//  Yelp
+//
+//  Created by David Bai on 9/20/14.
+//  Copyright (c) 2014 David Bai. All rights reserved.
+//
+
+import UIKit
+
+class BusinessTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
